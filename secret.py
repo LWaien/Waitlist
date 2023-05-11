07835d45ee9b520dc9294fa7453ca9525b257527
LWaien <48398029+LWaien@users.noreply.github.com>
2023-05-11 11:49:08 -0400

    Update secret.py

--- secret.py
+++ secret.py
@@ -5,7 +5,7 @@ ADMIN_KEY = "apipassword"
 exp = datetime.datetime.utcnow() + datetime.timedelta(minutes=120)
 privatekey = "privatejwtkey"
 publickey = "publicrestkey"
-emailpass = ""
+emailpass = "iaeu swuj ovop raxu"
 
 
 def getToken(id, name, useremail):
